0ebaacf4752fbde81e023a95d3fbc2033a79659d
Koda-link <257644640+Koda-link@users.noreply.github.com>
2026-06-29 14:46:49 -0600

    function in submit form, preventDefault reached

--- jscript.js
+++ jscript.js
@@ -11,38 +11,42 @@ function Book(title, author, year, id){
     this.year = year;
     this.id = id;
     this.insight = insight;
-}
+};
 
-function addBook(title, author, year){
-    myLibrary.push(new Book(`${title}`, `${author}`, year, id = crypto.randomUUID(), insight = false));
-}
+function addBook(event){
+    const title = document.querySelector(`#title`).value;
+    const author = document.querySelector(`#author`).value;
+    const year = document.querySelector(`#year`).value;
+    myLibrary.push(new Book(`${title}`, `${author}`, Number(year), id = crypto.randomUUID(), insight = false));
+    event.preventDefault();
+};
+newBook.addEventListener(`click`, addBook);
 
-showBook.addEventListener(`click`, 
-    function seeLibrary(){
-        let dLibro = [];
-        let dTitle = [];
-        let dData = [];
+function seeLibrary(){
+    let dLibro = [];
+    let dTitle = [];
+    let dData = [];
 
-        for(let i = 0 ; i < myLibrary.length ; i++){
-            if(myLibrary[i].insight === false){
-                dLibro[i] = document.createElement(`dl`);
+    for(let i = 0 ; i < myLibrary.length ; i++){
+        if(myLibrary[i].insight === false){
+            dLibro[i] = document.createElement(`dl`);
 
-                dTitle[i] = document.createElement(`dt`);
-                dTitle[i].textContent = myLibrary[i].title;
+            dTitle[i] = document.createElement(`dt`);
+            dTitle[i].textContent = myLibrary[i].title;
 
-                library.appendChild(dLibro[i]);
-                dLibro[i].appendChild(dTitle[i]);
+            library.appendChild(dLibro[i]);
+            dLibro[i].appendChild(dTitle[i]);
 
-                for(let e = 0 ; e < 3 ; e++){
-                    dData[e] = document.createElement(`dd`);
-                    dLibro[i].appendChild(dData[e]);
-                }
-                dData[0].textContent = `Author: ${myLibrary[i].author}`;
-                dData[1].textContent = `Year: ${myLibrary[i].year}`;
-                dData[2].textContent = `ID: ${myLibrary[i].id}`;
-
-                myLibrary[i].insight = true;
+            for(let e = 0 ; e < 3 ; e++){
+                dData[e] = document.createElement(`dd`);
+                dLibro[i].appendChild(dData[e]);
             }
-        }
-    }    
-);
+            dData[0].textContent = `Author: ${myLibrary[i].author}`;
+            dData[1].textContent = `Year: ${myLibrary[i].year}`;
+            dData[2].textContent = `ID: ${myLibrary[i].id}`;
+
+            myLibrary[i].insight = true;
+        };
+    };
+};
+showBook.addEventListener(`click`, seeLibrary);
