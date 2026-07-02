f15ece1891cb01e6621cd51e1bdb7f5719c5ba68
Koda-link <257644640+Koda-link@users.noreply.github.com>
2026-07-02 15:02:58 -0600

    added read status quality

--- jscript.js
+++ jscript.js
@@ -3,21 +3,26 @@ const showBook = document.querySelector(`#showBook`);
 const newBook = document.querySelector(`#newBook`);
 const myLibrary = [];
 
-function Book(title, author, year, id){
+function Book(title, author, year, pages, id){
     if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");}
     this.title = title;
     this.author = author;
     this.year = year;
+    this.pages = pages;
+
     this.id = id;
     this.insight = insight;
+    this.read = read;
 };
 
 function addBook(event){
     const title = document.querySelector(`#title`).value;
     const author = document.querySelector(`#author`).value;
     const year = document.querySelector(`#year`).value;
-    myLibrary.push(new Book(`${title}`, `${author}`, Number(year), id = crypto.randomUUID(), insight = false));
+    const pages = document.querySelector(`#pages`).value;
+
+    myLibrary.push(new Book(`${title}`, `${author}`, Number(year), Number(pages), id = crypto.randomUUID(), insight = false, read = false));
     event.preventDefault();
 };
 newBook.addEventListener(`click`, addBook);
@@ -28,6 +33,7 @@ function seeLibrary(){
     let dData = [];
 
     let dErase = [];
+    let dRead = [];
 
     for(let i = 0 ; i < myLibrary.length ; i++){
         if(myLibrary[i].insight === false){
@@ -44,8 +50,8 @@ function seeLibrary(){
                 dLibro[i].appendChild(dData[e]);
             }
             dData[0].textContent = `Author: ${myLibrary[i].author}`;
-            dData[1].textContent = `Year: ${myLibrary[i].year}`;
-            dData[2].textContent = `ID: ${myLibrary[i].id}`;
+            myLibrary[i].year < 0 ? dData[1].textContent = `Year: ${-(myLibrary[i].year)} AC` : dData[1].textContent = `Year: ${myLibrary[i].year}`;
+            dData[2].textContent = `Pages: ${myLibrary[i].pages}`;
 
             myLibrary[i].insight = true;
             dLibro[i].setAttribute(`data-id`, `${myLibrary[i].id}`);
@@ -58,9 +64,25 @@ function seeLibrary(){
                 library.removeChild(dLibro[i]);
                 myLibrary.splice(i, 1);
             });
+
+            dRead[i] = document.createElement(`button`);
+            dRead[i].textContent = `Read`;
+            dLibro[i].appendChild(dRead[i]);
+
+            dRead[i].addEventListener(`click`, Book.prototype.readBook = function (){
+                if(myLibrary[i].read == true){
+                    myLibrary[i].read = false;
+                    dRead[i].textContent = `Read`;
+                    dLibro[i].classList.remove(`read`);
+                }else{
+                    myLibrary[i].read = true;
+                    dRead[i].textContent = `unRead`;
+                    dLibro[i].classList.add(`read`);                    
+                }
+            });
         };
     };
 };
 showBook.addEventListener(`click`, seeLibrary);
 
-myLibrary.push(new Book(`flylord`, `goldie`, 1955, id = crypto.randomUUID(), insight = false));
+myLibrary.push(new Book(`flylord`, `goldie`, -1955, 125, id = crypto.randomUUID(), insight = false, read = false));
